afb578ebb674539acdc380ade36c8cb40583c9a7
Leo Vukoje <leo.vukoje02@gmail.com>
2023-09-14 07:26:03 +0200

    Prepare products feature for a discussion about untracked().

--- src/app/products/data-access/products-list.service.ts
+++ src/app/products/data-access/products-list.service.ts
@@ -5,55 +5,56 @@ import {first, map, Observable, Subject} from "rxjs";
 import {signal} from "@angular/core";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
-export interface ProductsListState {
+//Modified way
+/*export interface ProductsListState {
   products: WritableSignal<Product[]>;
   counter: WritableSignal<number>;
-}
+}*/
 
-/*export interface ProductsListState {
+export interface ProductsListState {
   products: Product[];
   counter: number;
-}*/
+}
 
 @Injectable({
   providedIn: 'root',
-
 })
 export class ProductsListService {
   private apiService: ProductsApiService = inject(ProductsApiService);
 
   // State
-  private state: ProductsListState = {
+  //Modified way
+  /*private state: ProductsListState = {
     products: signal<Product[]>([]),
     counter: signal<number>(0),
-  };
+  };*/
 
-  /*private state: WritableSignal<ProductsListState> = signal<ProductsListState>({
+  private state: WritableSignal<ProductsListState> = signal<ProductsListState>({
     products: [],
     counter: 0,
-  });*/
+  });
 
   // Selectors
-  /*public readonly products: Signal<Product[]> = computed(() => this.state().products);
-  public readonly counter: Signal<number> = computed(() => this.state().counter);*/
+  public readonly products: Signal<Product[]> = computed(() => this.state().products);
+  public readonly counter: Signal<number> = computed(() => this.state().counter);
 
-  public readonly products: Signal<Product[]> = this.state.products;
-  public readonly counter: Signal<number> = this.state.counter;
+  //Modified way
+  /*public readonly products: Signal<Product[]> = this.state.products;
+  public readonly counter: Signal<number> = this.state.counter;*/
 
   // Sources
   productsLoaded$: Observable<ProductsResponse> = this.apiService.get();
   counterIncrement$: Subject<number> = new Subject<number>();
 
   constructor() {
-    console.log("Service ctor")
     // Reducers
     this.productsLoaded$.pipe(first()).subscribe({
       next: (response: ProductsResponse) =>
-        this.state.products.set(response.products)
-      /*this.state.update(state => ({
+        /*this.state.products.set(response.products)*/
+      this.state.update(state => ({
         ...state,
         products: response.products
-      }))*/
+      }))
     })
 
     this.counterIncrement$
@@ -61,11 +62,11 @@ export class ProductsListService {
       .pipe(map(value => this.counter() + value))
       .subscribe({
       next: value =>
-        this.state.counter.set(value)
-        /*this.state.update(state => ({
+        /*this.state.counter.set(value)*/
+        this.state.update(state => ({
           ...state,
           counter: value
-        }))*/
+        }))
     })
   }
 }
--- src/app/products/features/products-list/products-list.component.ts
+++ src/app/products/features/products-list/products-list.component.ts
@@ -7,47 +7,26 @@ import {ProductsListService} from "../../data-access/products-list.service";
   styleUrls: ['./products-list.component.scss'],
   providers: [ProductsListService]
 })
-export class ProductsListComponent implements OnDestroy {
+export class ProductsListComponent {
   protected service: ProductsListService = inject(ProductsListService);
-  private injector: Injector = inject(Injector);
-
-  // Effect which can be manually destroyed, the other 2 ways of specifying an effect() handle destroying by themselves
-  /*private effectInDeclaration: EffectRef = effect(() => {
-    console.log(this.service.products());
-  })*/
 
   constructor() {
-    // Effect in constructor - most common
-    /*effect(() => {
-      console.log(this.service.products());
-    })*/
+    effect(() => {
+      // #1
+      // This will trigger effect if either of selectors change.
+      //console.log(`Products size is ${this.service.products().length} and counter is set to ${this.service.counter()}`);
 
-    effect((onCleanup) => {
+      // #2
+      // This should trigger only when products come in from server.
+      //console.log(`Products size is ${this.service.products().length} and counter is set to ${untracked(this.service.counter)}`);
 
-      // It is impossible to have untracked signal with SSSR architecture because our selectors get recomputed on change of any state property
-      // Therefore each selector will trigger every effect they are mentioned in
-      console.log(`Products size is ${this.service.products().length} and counter is set to ${this.service.counter()}`);
-
-      //a callback that is invoked before the next run of the effect begins, or when the effect is destroyed
-      onCleanup(() => {
-        console.log("Executing cleanup")
-      })
+      // #3
+      // This should also trigger only when products come in, but not when a counter increments.
+      console.log(`Products size is ${this.service.products().length}.`);
     })
   }
 
-  /*private effectInMethod(): void {
-    //Effect inside a class method required an injection context (access to the inject function)
-    effect(() => {
-      console.log(this.service.products());
-    }, {injector: this.injector})
-  }*/
-
   protected incrementCounter(): void {
     this.service.counterIncrement$.next(1);
-  }
-
-  ngOnDestroy() {
-    //Only possible with effects assigned to a field because those return an EffectRef which has a .destroy() method
-    //this.effectInDeclaration.destroy();
   }
 }
